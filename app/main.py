d55e77cb2c0e82ea17e43dcab9cca0c355fddfe6
Haris Athar <91695493+harisathar04@users.noreply.github.com>
2025-03-07 22:24:41 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,10 +3,10 @@ from app import app
 from app.model import analyze_sentiment
 from flask import render_template
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 
 @app.route("/predict", methods=["POST"])
@@ -17,5 +17,3 @@ def predict():
 
     sentiment = analyze_sentiment(data["text"])
     return jsonify({"sentiment": sentiment})
-
-
